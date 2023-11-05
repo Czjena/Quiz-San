95bc3ec0a6d754a798617ea2dd91f5e2bec9734a
Czjena <149963386+Czjena@users.noreply.github.com>
2023-11-05 20:09:29 +0100

    Add files via upload

---
+++ Quiz san/QuizSan/QuizSan/Backend/Question.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizSan.Backend
+{
+    internal class Question
+    {
+    }
+}
